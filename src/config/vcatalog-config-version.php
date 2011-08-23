c78fe78dbe198b0d73d5d2f5f4db619bafda93d3
btnguyen2k <btnguyen2k@724b9c5f-97f2-a51f-61a3-667660312ed5>
2011-08-23 15:32:57 +0000

    * Fixed a bug that causes duplicated search results

--- src/config/vcatalog-config-version.php
+++ src/config/vcatalog-config-version.php
@@ -1,4 +1,4 @@
 <?php
-$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.3.2.$Revision$');
+$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.3.1.$Revision$');
 define('VCATALOG_VERSION', $vCatalogVersion);
 //2011-08-18
