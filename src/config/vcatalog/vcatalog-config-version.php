b0bf9ab46d512b3fc98dd6546c0504084394424a
btnguyen2k <btnguyen2k@gmail.com>
2012-04-20 17:13:10 +0000

    * Add TextAds support

--- src/config/vcatalog/vcatalog-config-version.php
+++ src/config/vcatalog/vcatalog-config-version.php
@@ -1,7 +1,9 @@
 <?php
-$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.5.1.$Revision$');
+$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.5.2.$Revision$');
 define('VCATALOG_VERSION', $vCatalogVersion);
 /*
+ * * 2012-04-20: v0.5.1:
+ *   - Added "text ads" support
  * * 2012-03-16: v0.5.1
  *   - Added "new" item feature
  * * 2012-01-01: v0.5.0
