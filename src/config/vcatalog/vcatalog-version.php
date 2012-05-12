0c3491145ed5887bbfd932d6a8d6e5577a010a3f
btnguyen2k <btnguyen2k@gmail.com>
2012-05-12 05:03:17 +0000

    * v0.6.0: new backend system

--- src/config/vcatalog/vcatalog-version.php
+++ src/config/vcatalog/vcatalog-version.php
@@ -1,11 +1,16 @@
 <?php
-$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.5.2.$Revision$');
+$vCatalogVersion = preg_replace('/\\$Revision:.*?(\\d+).*?$/', '$1', 'v0.6.0.$Revision$');
 define('VCATALOG_VERSION', $vCatalogVersion);
 /*
+ * * 2012-05-12: v0.6.0
+ *   - New backend system
+ *
  * * 2012-04-20: v0.5.1:
  *   - Added "text ads" support
+ *
  * * 2012-03-16: v0.5.1
  *   - Added "new" item feature
+ *
  * * 2012-01-01: v0.5.0
  *   - Support paging when viewing category
  */
