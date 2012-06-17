8572fe48ec8c85f04744a8a1c10f9f2f987d4145
btnguyen2k <btnguyen2k@gmail.com>
2012-06-17 06:48:39 +0000

    * Added profiling table

--- dbschema/mysql/dbschema.mysql.sql
+++ dbschema/mysql/dbschema.mysql.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS app_profile_detail;
+DROP TABLE IF EXISTS app_profile;
 DROP TABLE IF EXISTS app_log;
 DROP TABLE IF EXISTS http_session;
 DROP TABLE IF EXISTS vcatalog_tag;
@@ -102,6 +104,23 @@ CREATE TABLE app_log(
     logMessage          TEXT,
     logStacktrace       TEXT,
     PRIMARY KEY (logid)
+) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE app_profile (
+    pid                 VARCHAR(16)         NOT NULL,
+    purl                VARCHAR(64),
+    ptimestamp          TIMESTAMP           NOT NULL,
+    pduration           DOUBLE              NOT NULL DEFAULT 0.0,
+    PRIMARY KEY (pid)
+) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE app_profile_detail (
+    pid                 VARCHAR(16)         NOT NULL,
+    pdid                VARCHAR(16)         NOT NULL,
+        INDEX (pid, pdid),
+    pdparent_id         VARCHAR(16),
+    pdname              VARCHAR(64),
+    pdduration          DOUBLE              NOT NULL DEFAULT 0.0
 ) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE http_session (
--- dbschema/mysql/patch.sql
+++ dbschema/mysql/patch.sql
@@ -1,40 +1,19 @@
-ALTER TABLE vcatalog_item ADD icode VARCHAR(32) AFTER ivendor, ADD INDEX (icode);
-UPDATE vcatalog_item SET icode=ivendor;
-UPDATE vcatalog_item SET ivendor=NULL;
+DROP TABLE IF EXISTS app_profile_detail;
+DROP TABLE IF EXISTS app_profile;
 
-DROP TABLE IF EXISTS gpv_product;
-DROP TABLE IF EXISTS gpv_site;
+CREATE TABLE app_profile (
+    pid                 VARCHAR(16)         NOT NULL,
+    purl                VARCHAR(64),
+    ptimestamp          TIMESTAMP           NOT NULL,
+    pduration           DOUBLE              NOT NULL DEFAULT 0.0,
+    PRIMARY KEY (pid)
+) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-CREATE TABLE gpv_site (
-    sdomain                 VARCHAR(128)            NOT NULL,
-    sref                    VARCHAR(128),
-        INDEX (sref),
-    stimestamp              INT                     NOT NULL DEFAULT 0,
-        INDEX (stimestamp),
-    scustomer_id            VARCHAR(64),
-        INDEX (scustomer_id),
-    PRIMARY KEY (sdomain),
-    FOREIGN KEY (sref) REFERENCES gpv_site(sdomain) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-INSERT INTO gpv_site (sdomain, sref, stimestamp) VALUES ('localhost', NULL, UNIX_TIMESTAMP());
-INSERT INTO gpv_site (sdomain, sref, stimestamp) VALUES ('localhost.localdomain', 'localhost', UNIX_TIMESTAMP());
-INSERT INTO gpv_site (sdomain, sref, stimestamp) VALUES ('127.0.0.1', 'localhost', UNIX_TIMESTAMP());
-
-CREATE TABLE gpv_product (
-    psite_domain            VARCHAR(128)            NOT NULL,
-        INDEX (psite_domain),
-        FOREIGN KEY (psite_domain) REFERENCES gpv_site(sdomain) ON DELETE CASCADE ON UPDATE CASCADE,
-    pname                   VARCHAR(32)             NOT NULL,
-    pversion_1              TINYINT                 NOT NULL DEFAULT 0,
-    pversion_2              TINYINT                 NOT NULL DEFAULT 0,
-    pversion_3              TINYINT                 NOT NULL DEFAULT 0,
-    pversion_4              TINYINT                 NOT NULL DEFAULT 0,
-        INDEX (pversion_1, pversion_2, pversion_3, pversion_4),
-    plevel                  INT                     NOT NULL DEFAULT 0,
-    ptimestamp              INT                     NOT NULL DEFAULT 0,
-    pexpiry                 INT                     NOT NULL DEFAULT 0,
-    pconfig                 MEDIUMTEXT,
-    PRIMARY KEY (psite_domain, pname)
-) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-INSERT INTO gpv_product (psite_domain, pname, plevel, ptimestamp, pexpiry, pversion_1, pversion_2, pversion_3, pconfig)
-VALUES ('localhost', 'VCATALOG', 0, UNIX_TIMESTAMP(), -1, 0, 6, 0, '{"DB":{"TYPE":"MYSQL", "HOST":"localhost", "USER":"vcatalog_localhost", "PASSWORD":"vcatalog_localhost", "DATABASE":"vcatalog_localhost", "SETUP_SQLS":["SET NAMES ''utf8''"]}}');
+CREATE TABLE app_profile_detail (
+    pid                 VARCHAR(16)         NOT NULL,
+    pdid                VARCHAR(16)         NOT NULL,
+        INDEX (pid, pdid),
+    pdparent_id         VARCHAR(16),
+    pdname              VARCHAR(64),
+    pdduration          DOUBLE              NOT NULL DEFAULT 0.0
+) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
