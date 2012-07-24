053e4ca4ffca58df55a38cc92b59ef4e69e75a29
btnguyen2k <btnguyen2k@724b9c5f-97f2-a51f-61a3-667660312ed5>
2012-07-24 07:54:57 +0000

    * Refactor bootstrap.php

--- src/modules/_0_bootstrap/bootstrap.php
+++ src/modules/_0_bootstrap/bootstrap.php
@@ -2,85 +2,147 @@
 /* vuShop's global boostrap file */
 
 // disable cache
-#global $DPHP_CACHE_CONFIG;
-#global $DPHP_CACHE_CONFIG_MEMORY;
-#$DPHP_CACHE_CONFIG = &$DPHP_CACHE_CONFIG_MEMORY;
+//global $DPHP_CACHE_CONFIG;
+//global $DPHP_CACHE_CONFIG_MEMORY;
+//$DPHP_CACHE_CONFIG = &$DPHP_CACHE_CONFIG_MEMORY;
 
-// temporarily switch to simple log
-global $DPHP_COMMONS_LOGGING_CONFIG_SIMPLE;
-global $DPHP_COMMONS_LOGGING_CONFIG_MYSQL;
-global $DPHP_COMMONS_LOGGING_CONFIG;
-$SAVE_LOGGING_CONFIG= &$DPHP_COMMONS_LOGGING_CONFIG;
-$DPHP_COMMONS_LOGGING_CONFIG = &$DPHP_COMMONS_LOGGING_CONFIG_SIMPLE;
-
-global $DPHP_DAO_CONFIG_MYSQL_SITE;
-$siteDao = Ddth_Dao_BaseDaoFactory::getInstance($DPHP_DAO_CONFIG_MYSQL_SITE)->getDao(DAO_SITE);
-$tokens = explode(":", $_SERVER["HTTP_HOST"]);
-$host = trim($tokens[0]);
-$site = $siteDao->getSiteByDomain($host);
-if ($site === NULL) {
-    die("Domain '{$_SERVER["HTTP_HOST"]}' does not exist!");
-}
-$product = $site->getProduct(PRODUCT_NAME);
-if ($product === NULL) {
-    die("Product '{PRODUCT_NAME}' does not exist!");
-}
-if ($product->isExpired()) {
-    die("Product '{PRODUCT_NAME}' has expired!");
-}
-$prodConfig = $product->getProductConfigMap();
-
-// switch back to configured logging
-$DPHP_COMMONS_LOGGING_CONFIG = &$SAVE_LOGGING_CONFIG;
-
-// setup logger
-if (strtoupper($prodConfig['DB']['TYPE']) == 'MYSQL') {
-    global $DPHP_COMMONS_LOGGING_CONFIG_MYSQL;
-    $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.host'] = $prodConfig['DB']['HOST'];
-    $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.username'] = $prodConfig['DB']['USER'];
-    $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.password'] = $prodConfig['DB']['PASSWORD'];
-    $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.dbname'] = $prodConfig['DB']['DATABASE'];
-    $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.setupSqls'] = $prodConfig['DB']['SETUP_SQLS'];
-} else {
-    die("Db type {$prodConfig['DB']['TYPE']} is not supported!");
+function bootstrap_getSiteInfo() {
+    global $DPHP_DAO_CONFIG_SITE;
+    $siteDao = Ddth_Dao_BaseDaoFactory::getInstance($DPHP_DAO_CONFIG_SITE)->getDao(DAO_SITE);
+    $tokens = explode(":", $_SERVER["HTTP_HOST"]);
+    $host = trim($tokens[0]);
+    $site = $siteDao->getSiteByDomain($host);
+    return $site;
 }
 
-// setup DAOs
-if (strtoupper($prodConfig['DB']['TYPE']) == 'MYSQL') {
-    global $DPHP_DAO_CONFIG_MYSQL;
-    $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_HOST] = $prodConfig['DB']['HOST'];
-    $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_USERNAME] = $prodConfig['DB']['USER'];
-    $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_PASSWORD] = $prodConfig['DB']['PASSWORD'];
-    $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_DATABASE] = $prodConfig['DB']['DATABASE'];
-    $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_SETUP_SQLS] = $prodConfig['DB']['SETUP_SQLS'];
-} else {
-    die("Db type {$prodConfig['DB']['TYPE']} is not supported!");
+function bootstrap_getProductConfig() {
+    $site = bootstrap_getSiteInfo();
+    if ($site === NULL) {
+        die("Domain '{$_SERVER["HTTP_HOST"]}' does not exist!");
+    }
+    $product = $site->getProduct(PRODUCT_CODE);
+    if ($product === NULL) {
+        die("Product '" . PRODUCT_CODE . "' does not exist!");
+    }
+    if ($product->isExpired()) {
+        die("Product '" . PRODUCT_CODE . "' has expired!");
+    }
+    $prodConfig = $product->getProductConfigMap();
+    return $prodConfig;
 }
 
-if (!defined('SKIN_DIR_BACKEND')) {
-    // if it's not backend, we switch to user-defined skin if possible
-    $configDao = Ddth_Dao_BaseDaoFactory::getInstance()->getDao(DAO_CONFIG);
-    $siteSkin = $configDao->loadConfig(CONFIG_SITE_SKIN);
-    if ($siteSkin == NULL || $siteSkin->getValue() == '') {
-        $siteSkin = 'default';
+function bootstrap_configureSiteLogging() {
+    // temporarily switch to simple log
+    global $DPHP_COMMONS_LOGGING_CONFIG_SIMPLE;
+    global $DPHP_COMMONS_LOGGING_CONFIG;
+    $SAVE_LOGGING_CONFIG = &$DPHP_COMMONS_LOGGING_CONFIG;
+    $DPHP_COMMONS_LOGGING_CONFIG = &$DPHP_COMMONS_LOGGING_CONFIG_SIMPLE;
+
+    // pre-open db connection(s) for latter use
+    global $DPHP_DAO_CONFIG_SITE;
+    $_dao = Ddth_Dao_BaseDaoFactory::getInstance($DPHP_DAO_CONFIG_SITE)->getDao('dao._');
+    $_dao->getConnection();
+
+    $prodConfig = bootstrap_getProductConfig();
+
+    // setup logger
+    if (strtoupper($prodConfig['DB']['TYPE']) == 'MYSQL') {
+        global $DPHP_COMMONS_LOGGING_CONFIG_MYSQL;
+        $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.host'] = $prodConfig['DB']['HOST'];
+        $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.username'] = $prodConfig['DB']['USER'];
+        $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.password'] = $prodConfig['DB']['PASSWORD'];
+        $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.dbname'] = $prodConfig['DB']['DATABASE'];
+        $DPHP_COMMONS_LOGGING_CONFIG_MYSQL['logger.setting.mysql.setupSqls'] = $prodConfig['DB']['SETUP_SQLS'];
+    } else if (strtoupper($prodConfig['DB']['TYPE']) == 'PGSQL') {
+        global $DPHP_COMMONS_LOGGING_CONFIG_PGSQL;
+        $DPHP_COMMONS_LOGGING_CONFIG_PGSQL['logger.setting.pgsql.host'] = $prodConfig['DB']['HOST'];
+        $DPHP_COMMONS_LOGGING_CONFIG_PGSQL['logger.setting.pgsql.username'] = $prodConfig['DB']['USER'];
+        $DPHP_COMMONS_LOGGING_CONFIG_PGSQL['logger.setting.pgsql.password'] = $prodConfig['DB']['PASSWORD'];
+        $DPHP_COMMONS_LOGGING_CONFIG_PGSQL['logger.setting.pgsql.dbname'] = $prodConfig['DB']['DATABASE'];
+    } else {
+        die("Db type {$prodConfig['DB']['TYPE']} is not supported!");
     }
 
-    $skinDir = SITE_SKINS_ROOT_DIR . "$siteSkin/";
-    define('SKIN_DIR_EX', $skinDir);
-    $params = Array('templateDir' => $skinDir,
-            'prefix' => 'page_',
-            'suffix' => '.tpl',
-            'smartyCacheDir' => '../../../smarty/cache',
-            'smartyConfigDir' => '../../../smarty/config',
-            'smartyCompileDir' => '../../../smarty/template_c',
-            'smartyLeftDelimiter' => '[:',
-            'smartyRightDelimiter' => ':]');
-    $viewResolverClass = 'Dzit_View_SmartyViewResolver';
-    Dzit_Config::set(Dzit_Config::CONF_VIEW_RESOLVER, new $viewResolverClass($params));
+    // switch back to configured logging
+    $DPHP_COMMONS_LOGGING_CONFIG = &$SAVE_LOGGING_CONFIG;
 }
+
+function bootstrap_configureDAOs() {
+    $prodConfig = bootstrap_getProductConfig();
+
+    // setup DAOs
+    if (strtoupper($prodConfig['DB']['TYPE']) == 'MYSQL') {
+        global $DPHP_DAO_CONFIG_MYSQL;
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_HOST] = $prodConfig['DB']['HOST'];
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_DATABASE] = $prodConfig['DB']['DATABASE'];
+
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_PORT] = isset($prodConfig['DB']['PORT']) ? $prodConfig['DB']['PORT'] : NULL;
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_USERNAME] = isset($prodConfig['DB']['USER']) ? $prodConfig['DB']['USER'] : NULL;
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_MYSQL_PASSWORD] = isset($prodConfig['DB']['PASSWORD']) ? $prodConfig['DB']['PASSWORD'] : NULL;
+
+        $DPHP_DAO_CONFIG_MYSQL[Ddth_Dao_Mysql_BaseMysqlDaoFactory::CONF_SETUP_SQLS] = isset($prodConfig['DB']['SETUP_SQLS']) ? $prodConfig['DB']['SETUP_SQLS'] : NULL;
+    } elseif (strtoupper($prodConfig['DB']['TYPE']) == 'PGSQL') {
+        global $DPHP_DAO_CONFIG_PGSQL;
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_CONNECTION_STRING] = isset($prodConfig['DB']['CONNECTION_STRING']) ? $prodConfig['DB']['CONNECTION_STRING'] : NULL;
+
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_HOST] = isset($prodConfig['DB']['HOST']) ? $prodConfig['DB']['HOST'] : NULL;
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_DATABASE] = isset($prodConfig['DB']['DATABASE']) ? $prodConfig['DB']['DATABASE'] : NULL;
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_PORT] = isset($prodConfig['DB']['PORT']) ? $prodConfig['DB']['PORT'] : NULL;
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_USERNAME] = isset($prodConfig['DB']['USER']) ? $prodConfig['DB']['USER'] : NULL;
+        $DPHP_DAO_CONFIG_PGSQL[Ddth_Dao_Pgsql_BasePgsqlDaoFactory::CONF_PGSQL_PASSWORD] = isset($prodConfig['DB']['PASSWORD']) ? $prodConfig['DB']['PASSWORD'] : NULL;
+    } else {
+        die("Db type {$prodConfig['DB']['TYPE']} is not supported!");
+    }
+
+    // pre-open db connection(s) for latter use
+    $_dao = Ddth_Dao_BaseDaoFactory::getInstance()->getDao('dao._');
+    $_dao->getConnection();
+}
+
+function bootstrap_configSkin() {
+    if (!defined('SKIN_DIR_BACKEND')) {
+        // if it's not backend, we switch to user-defined skin if possible
+        $configDao = Ddth_Dao_BaseDaoFactory::getInstance()->getDao(DAO_CONFIG);
+        $siteSkin = $configDao->loadConfig(CONFIG_SITE_SKIN);
+        if ($siteSkin == NULL || $siteSkin->getValue() == '') {
+            $siteSkin = 'default';
+        } else {
+            $siteSkin = $siteSkin->getValue();
+        }
+
+        $skinDir = SITE_SKINS_ROOT_DIR . $siteSkin . "/";
+        if (!is_dir($skinDir)) {
+            // fallback to default skin
+            $skinDir = SITE_SKINS_ROOT_DIR . "default/";
+        }
+        define('SKIN_DIR_EX', $skinDir);
+        $params = Array('templateDir' => $skinDir,
+                'prefix' => 'page_',
+                'suffix' => '.tpl',
+                'smartyCacheDir' => '../../../smarty/cache',
+                'smartyConfigDir' => '../../../smarty/config',
+                'smartyCompileDir' => '../../../smarty/template_c',
+                'smartyLeftDelimiter' => '[:',
+                'smartyRightDelimiter' => ':]');
+        $viewResolverClass = 'Dzit_View_SmartyViewResolver';
+        Dzit_Config::set(Dzit_Config::CONF_VIEW_RESOLVER, new $viewResolverClass($params));
+    }
+}
+
+function bootstrap_cleanupCart() {
+    if (rand(0, 10) < 2) {
+        $cartDao = Ddth_Dao_BaseDaoFactory::getInstance()->getDao(DAO_CART);
+        $cartDao->cleanup();
+    }
+}
+
+bootstrap_configureSiteLogging();
+bootstrap_configureDAOs();
+bootstrap_configSkin();
+bootstrap_cleanupCart();
 
 /*
  * Use Quack's session handler. Comment the next line if you want to use PHP's
- * default session handler
- */
+* default session handler
+*/
 Quack_SessionS_SessionHandler::startSession(DAO_SESSION);
